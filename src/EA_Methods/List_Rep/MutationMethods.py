7c75bae5306312fd429b20210def765c3e19a71e
Zachary Northcott <zmnorthcott@mun.ca>
2018-11-08 12:22:37 -0330

    Made a few changes. Of note, and weirdly, the list brute force seems to run faster than the numpy one. To be investigated. The pandas implementation framework is effectively complete, though still optimizable. Speed test comparisons are in order soon, after implementation of GA methods in the EA_Methods folder. TODO: Finish implementing crossover methods in list and pandas and mutation methods in pandas methods. TODO: Further improve testbed output, for algorithm comparisons.

--- src/EA_Methods/List_Rep/MutationMethods.py
+++ src/EA_Methods/List_Rep/MutationMethods.py
@@ -27,23 +27,26 @@ def method_randomizer(func):
     return select_method
 
 
-def gen_two_nums():
+def gen_two_nums(substring):
     x = randint(0, genome_length - 1)
     y = (x + randint(1, genome_length - 1)) % genome_length
     if x > y: x, y = y, x
-    return x, y
+    if substring and y - x == 1:
+        if y != genome_length - 1: return x, y + 1
+        else: return x - 1, y
+    else: return x, y
 
 
 @method_randomizer
 def permutation_swap(individual):
-    x, y = gen_two_nums()
+    x, y = gen_two_nums(False)
     individual[x], individual[y] = individual[y], individual[x]
     return individual
 
 
 @method_randomizer
 def permutation_insert(individual):
-    x, y = gen_two_nums()
+    x, y = gen_two_nums(False)
     ele = individual.pop(y)
     individual.insert(x, ele)
     return individual
@@ -51,15 +54,13 @@ def permutation_insert(individual):
 
 @method_randomizer
 def permutation_inversion(individual):
-    x, y = gen_two_nums()
-    if y - x == 1: y += 1
+    x, y = gen_two_nums(True)
     return individual[:x] + individual[y-1:x-1:-1] + individual[y:]
 
 
 @method_randomizer
 def permutation_scramble(individual):
-    x, y = gen_two_nums()
-    if y - x == 1: y += 1
+    x, y = gen_two_nums(True)
     temp = individual[x:y]
     shuffle(temp)
     return individual[:x] + temp + individual[y:]
@@ -70,7 +71,7 @@ if __name__ == '__main__':
     genome_length = 10
     mutation_rate = 1
     test = [sample([c for c in range(genome_length)], genome_length) for _ in range(genome_length)]
-    for x in test:
-        print(x)
-        print(permutation_scramble(x))
+    for i in test:
+        print(i)
+        print(permutation_scramble(i))
         print('- - -')
