86aed68529f1372b52f60067cb3d29711fb6b6ad
Zachary Northcott <zmnorthcott@mun.ca>
2018-12-02 23:24:33 -0330

    Changed the decorators to wrapping functions due to limitations within the classes.
    Signed-off-by: Zachary Northcott <zmnorthcott@mun.ca>

--- src/EA_Methods/ParentSelectionMethods.py
+++ src/EA_Methods/ParentSelectionMethods.py
@@ -4,9 +4,9 @@ from random import sample, random, shuffle
 
 class ParentSelectionHelper(BaseHelper):
     def __init__(self, var_helper, data_type):
-        name_method_pairs = [('MPS', self.mps),
-                             ('Tourney', self.tournament),
-                             ('Random', self.random_uniform)
+        name_method_pairs = [('MPS', self.select_parents_using(self.mps)),
+                             ('Tourney', self.select_parents_using(self.tournament)),
+                             ('Random', self.select_parents_using(self.random_uniform))
                              ]
         super().__init__(var_helper, data_type, name_method_pairs)
 
@@ -15,22 +15,21 @@ class ParentSelectionHelper(BaseHelper):
 
     # region Parent Selection Methods
     # Takes in a method, and then 'injects' the code to shuffle the output.
-    @staticmethod
-    def randomize_output(func):
-        def generate_output(fitness, mating_pool_size):
+    def select_parents_using(self, func):
+        def shuffle_output(fitness):
             # Find the parents,
-            selected_to_mate = func(fitness, mating_pool_size)
+            selected_to_mate = func(fitness)
             # And then shuffle their indices.
             shuffle(selected_to_mate)
             return selected_to_mate
 
-        return generate_output
+        shuffle_output.__name__ = func.__name__
+        return shuffle_output
 
-    @randomize_output
-    def mps(self, fitness, mating_pool_size):
+    def mps(self, fitness):
         selected_to_mate = []  # a list of indices of picked parents in population
         total_fitness = sum(fitness)
-        increment = 1 / mating_pool_size  # The pointer 'angle'
+        increment = 1 / self.vars.mating_pool_size  # The pointer 'angle'
         seed = random() / len(fitness)  # The pointer start position
         a = 0  # The rolling probability sum
 
@@ -48,17 +47,15 @@ class ParentSelectionHelper(BaseHelper):
 
         return selected_to_mate
 
-    @randomize_output
-    def tournament(self, fitness, mating_pool_size):
+    def tournament(self, fitness):
         # Generate tuples of index-fitness pairs.
         fit_indexes = [(x, fitness[x]) for x in range(len(fitness))]
 
         # Return a list of parents indices based of the winners of the tournaments.
         return [self.vars.op(sample(fit_indexes, self.vars.tournament_size), key=lambda x: x[1])[0] for _ in
-                range(mating_pool_size)]
+                range(self.vars.mating_pool_size)]
 
-    @randomize_output
-    def random_uniform(self, fitness, mating_pool_size):
+    def random_uniform(self, fitness):
         # Return a number of parent indices selected at random.
-        return sample([x for x in range(len(fitness))], mating_pool_size)
+        return sample([x for x in range(len(fitness))], self.vars.mating_pool_size)
     # endregion
